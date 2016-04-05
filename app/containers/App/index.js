fedc23de98da2959053d9171ae512f9a42e58fbc
Dylan Smith <dylantsmith@gmail.com>
2016-04-04 21:46:25 -0500

    Add web font
    Add a web font for the project, include it in the HTML body of the
    application.

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import './style.scss';
+
 class App extends Component {
   render() {
     const { children } = this.props;
