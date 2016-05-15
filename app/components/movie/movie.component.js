1be57eb2387540d4fec66cc867e7717a5ea08a6d
Dylan Smith <dylantsmith@gmail.com>
2016-05-14 21:36:31 -0500

    Support synopsis within Movie
    Add support to display the movie synopsis within the Movie component.
    This required converting the Movie component into a stateful component
    because the synopsis display toggles to display the full text.  So the
    majority of the changes in the Movie component is whitespace changes.
    
    Some additional styling changes required a new swatch.

--- app/components/movie/movie.component.js
+++ app/components/movie/movie.component.js
@@ -1,63 +1,92 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 import style from './movie.component.scss';
 
-export default function Movie(props) {
-  function saveMovie() {
-    return props.saveMovie(props.id);
+export default class Movie extends Component {
+  state = {
+    synopsisContentWrapperClass: style.synopsisContentWrapper,
+    synopsisMoreClass: style.synopsisMore,
+  };
+
+  saveMovie() {
+    return this.props.saveMovie(this.props.id);
   }
 
-  function dismissMovie() {
-    return props.dismissMovie(props.id);
+  dismissMovie() {
+    return this.props.dismissMovie(this.props.id);
   }
 
-  function undismissMovie() {
-    return props.undismissMovie(props.id);
+  undismissMovie() {
+    return this.props.undismissMovie(this.props.id);
   }
 
-  let saveButton;
-  if (!props.saved && !props.dismissed) {
-    saveButton = <button className={style.button} onClick={saveMovie}>Save</button>;
+  showMore() {
+    this.setState({
+      synopsisContentWrapperClass: `${this.state.synopsisContentWrapperClass} ` +
+        `${style.showMore}`,
+      synopsisMoreClass: `${this.state.synopsisMoreClass} ${style.hideMore}`,
+    });
   }
-  let dismissButton;
-  if (!props.dismissed) {
-    dismissButton = <button className={style.button} onClick={dismissMovie}>Dismiss</button>;
-  }
-  let undismissButton;
-  if (props.dismissed) {
-    undismissButton = (
-      <button className={style.button} onClick={undismissMovie}>Undo Dismiss</button>
+
+  render() {
+    let saveButton;
+    if (!this.props.saved && !this.props.dismissed) {
+      saveButton = (
+        <button className={style.button} onClick={::this.saveMovie}>Save</button>
+      );
+    }
+
+    let dismissButton;
+    if (!this.props.dismissed) {
+      dismissButton = (
+        <button className={style.button} onClick={::this.dismissMovie}>Dismiss</button>
+      );
+    }
+
+    let undismissButton;
+    if (this.props.dismissed) {
+      undismissButton = (
+        <button className={style.button} onClick={::this.undismissMovie}>Undo Dismiss</button>
+      );
+    }
+
+    const buttons = (
+      <div className={style.buttons}>
+        {saveButton}
+        {dismissButton}
+        {undismissButton}
+      </div>
+    );
+
+    const criticIcon = style[this.props.tomatoIcon];
+    const userIcon = this.props.userScore >= 70 ? style.popcorn : style.spilled;
+
+    return (
+      <div className={style.movie}>
+        <img className={style.image} src={this.props.image} role="presentation" />
+        <div className={style.title}>{this.props.title}</div>
+        <div className={style.scores}>
+          <i className={criticIcon}></i>
+          <span className={style.score}>{this.props.criticScore}</span>
+          <i className={userIcon}></i>
+          <span className={style.score}>{this.props.userScore}</span>
+        </div>
+        <div className={style.attributes}>
+          <span className={style.attribute}>{this.props.runtime}</span>
+          <span className={style.attribute}>{this.props.mpaaRating}</span>
+        </div>
+        <div className={style.synopsis}>
+          <div className={this.state.synopsisContentWrapperClass}>
+            <p className={style.synopsisContent}>
+              {this.props.synopsis}
+            </p>
+          </div>
+          <div className={this.state.synopsisMoreClass} onClick={::this.showMore}>show more</div>
+        </div>
+        {buttons}
+      </div>
     );
   }
-
-  const buttons = (
-    <div className={style.buttons}>
-      {saveButton}
-      {dismissButton}
-      {undismissButton}
-    </div>
-  );
-
-  const criticIcon = style[props.tomatoIcon];
-  const userIcon = props.userScore >= 70 ? style.popcorn : style.spilled;
-
-  return (
-    <div className={style.movie}>
-      <img className={style.image} src={props.image} role="presentation" />
-      <div className={style.title}>{props.title}</div>
-      <div className={style.scores}>
-        <i className={criticIcon}></i>
-        <span className={style.score}>{props.criticScore}</span>
-        <i className={userIcon}></i>
-        <span className={style.score}>{props.userScore}</span>
-      </div>
-      <div className={style.attributes}>
-        <span className={style.attribute}>{props.runtime}</span>
-        <span className={style.attribute}>{props.mpaaRating}</span>
-      </div>
-      {buttons}
-    </div>
-  );
 }
 
 Movie.propTypes = {
@@ -69,6 +98,7 @@ Movie.propTypes = {
   tomatoIcon: PropTypes.string,
   mpaaRating: PropTypes.string.isRequired,
   runtime: PropTypes.string.isRequired,
+  synopsis: PropTypes.string.isRequired,
   saved: PropTypes.bool.isRequired,
   dismissed: PropTypes.bool.isRequired,
   saveMovie: PropTypes.func.isRequired,
