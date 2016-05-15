eb6cc2fa96b7169ce5e97526c29d81aa4d67b8b4
Dylan Smith <dylantsmith@gmail.com>
2016-05-14 21:41:48 -0500

    Add Movie component to the Test App
    Add a movie test container which renders many Movie components to view
    the various states of the component.  Add this container to the main
    test app index and home page links.

--- app/test-app-index.js
+++ app/test-app-index.js
@@ -4,11 +4,13 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import TestApp from './containers/test/test-app/test-app.container';
 import TestHomeContainer from './containers/test/test-home/test-home.container';
+import MovieTestContainer from './containers/test/movie-test/movie-test.container';
 
 render(
   <Router history={browserHistory}>
     <Route path="/" component={TestApp}>
       <IndexRoute component={TestHomeContainer} />
+      <Route path="/movie" component={MovieTestContainer} />
     </Route>
   </Router>,
   document.getElementById('app')
